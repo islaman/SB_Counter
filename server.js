7edcaa80a69ace7da1ebb222a798bea5581ae0fe
Francis S <125688765+islaman@users.noreply.github.com>
2025-01-05 18:26:49 +0000

    Ajuste de rutas y estructura del servidor 6

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const salesRouter = require('./routes/sales'); // Importa las rutas
+const salesRouter = require('./server/routes/sales'); // Importa las rutas
 
 const app = express();
 app.use(express.json());
